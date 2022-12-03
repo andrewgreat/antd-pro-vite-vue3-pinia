196b6e8aeaf5ea294020c4ca2b5c331e0f3345a8
andrewgreat <31468559@qq.com>
2022-12-03 20:59:28 +0800

    fix issue for bootstrap

--- src/core/bootstrap.ts
+++ src/core/bootstrap.ts
@@ -4,19 +4,19 @@ import defaultSettings from "@/config/defaultSettings";
 
 export default function Initializer() {
   console.log("main.ts...", "bootstrap");
-  let app = useAppStore();
-  app.layout = storage.get("layout", defaultSettings.layout);
-  app.fixedHeader = storage.get("fixed_header", defaultSettings.layout);
-  app.fixSiderbar = storage.get("fixed_sidebar", defaultSettings.layout);
-  app.contentWidth = storage.get("content_width", defaultSettings.layout);
-  app.autoHideHeader = storage.get("auto_hide_header", defaultSettings.layout);
-  app.navTheme = storage.get("nav_theme", defaultSettings.layout);
-  app.colorWeak = storage.get("color_weak", defaultSettings.layout);
-  app.primaryColor = storage.get("primary_color", defaultSettings.layout);
-  app.multiTab = storage.get("multi_tab", defaultSettings.layout);
-  app.splitMenus = storage.get("split_menus", defaultSettings.layout);
+  const appStore = useAppStore();
+  appStore.layout = storage.get("layout", defaultSettings.layout);
+  appStore.fixedHeader = storage.get("fixed_header", defaultSettings.fixedHeader);
+  appStore.fixSiderbar = storage.get("fixed_sidebar", defaultSettings.fixSiderbar);
+  appStore.contentWidth = storage.get("content_width", defaultSettings.contentWidth);
+  appStore.autoHideHeader = storage.get("auto_hide_header", defaultSettings.autoHideHeader);
+  appStore.navTheme = storage.get("nav_theme", defaultSettings.navTheme);
+  appStore.colorWeak = storage.get("color_weak", defaultSettings.colorWeak);
+  appStore.primaryColor = storage.get("primary_color", defaultSettings.primaryColor);
+  appStore.multiTab = storage.get("multi_tab", defaultSettings.multiTab);
+  appStore.splitMenus = storage.get("split_menus", defaultSettings.splitMenus);
   // store.commit('SET_TOKEN', storage.get(ACCESS_TOKEN))
 
-  app.setLang(storage.get("app_language", "en-US")).then(() => {});
+  appStore.setLang(storage.get("app_language", "en-US")).then(() => {});
   // last step
 }
